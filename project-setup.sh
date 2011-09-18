9d3dfeef311af3c1acd9206bf450e5e3c06f7055
filtercake <mail@fabianfabian.net>
2011-09-18 20:50:37 +0200

    added heroku, fixed RVM as a function into local environment

--- project-setup.sh
+++ project-setup.sh
@@ -1,49 +1,52 @@
 #! /bin/sh
 
 
+# IMPORTANT: Source RVM as a function into local environment.
+#            Otherwise switching gemsets won't work.
+[ -s "$HOME/.rvm/scripts/rvm" ] && . "$HOME/.rvm/scripts/rvm"
+
+# see http://stackoverflow.com/questions/5792707/cannot-change-rvm-gemset-from-shell-script-via-rvm-gemset-use/6584010#6584010
+
+
 echo " "
 echo "enter project name or working title"
 read projectname
-echo "you entered: $projectname"
 mkdir ~/projects/$projectname
 echo "created directory ~/projects/$projectname"
 cd ~/projects/$projectname
-touch _notes-$projectname.txt
-echo "created _notes-$projectname.txt"
-touch readme
+touch notes-$projectname.txt
+echo "created notes-$projectname.txt"
+touch readme.txt
 echo "$projectname by $USER" >> readme
 date >> readme
-echo "created readme"
+echo "created readme.txt"
 mkdir copy conception design archive doc research temp
 echo "created sub directories: copy conception design  archive doc research temp"
 echo " "
-echo "will now create gemset and .rvmrc file"
-cd deploy
 rvm list
-echo "which ruby version do you want to use?"
+echo "which ruby?"
 read ruby_ver
 rvm use $ruby_ver
-echo "using $ruby_ver"
+# echo "using $ruby_ver"
 rvm gemset create $projectname
-echo "created gemset $projectname"
-# read temp
 rvm use ruby_ver@$projectname
-# echo "using gemset $projectname?"
-# rvm gemset list
-# gem list
-echo "installing bundler, rake"
-gem install bundler rake --no-rdoc --no-ri
-echo "installed bundler and rake"
-
-
+echo "installing bundler, rake, heroku"
+gem install bundler rake heroku --no-rdoc --no-ri
+heroku keys:add
+echo " "
+# rvm gemset list   # for debugging
+# gem list 			# for debugging
+echo " "
 
 cd ~/projects/$projectname
 rvm --rvmrc --create $ruby_ver@$projectname
+echo " " >> .rvmrc
+echo "rvm list" >> .rvmrc
+echo "rvm gemset list" >> .rvmrc
 echo "created .rvmrc file in $projectname directory"
-
-
 echo ""
-echo "your project $projectname was set up."
+
+echo "your project $projectname is set up."
 echo "try:"
 echo ""
 echo "cd ~/projects/$projectname"
--- readme.md
+++ readme.md
@@ -1,4 +1,18 @@
 
-# project setup
+# what does it do?
 
-this script sets up a folder- and filestructure in ~/projects/my-shiny-new-project, creates gemset, installs rake and bundler, and writes .rvmrc, notes and readme files in the projects base directory.
+* asks for a name or working title for the project
+* sets up folderstructure in ~/projects/my-shiny-new-project
+* creates gemset
+* installs rake, bundler, heroku
+* adds public key to heroku
+* writes .rvmrc, notes and readme files in the projects base directory.
+
+the idea is to have *some* rvm-enviroment, whatever happens…
+
+# thanks
+
+[dadooda][1]
+
+[1]: http://stackoverflow.com/questions/5792707/cannot-change-rvm-gemset-from-shell-script-via-rvm-gemset-use/6584010#6584010
+
