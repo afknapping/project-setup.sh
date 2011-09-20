36bd214ac69e58775dffc2c6a83ed65df2d9921b
filtercake <mail@fabianfabian.net>
2011-09-20 19:53:10 +0200

    added rawmaterial folder.

--- project-setup.sh
+++ project-setup.sh
@@ -7,9 +7,9 @@
 
 # see http://stackoverflow.com/questions/5792707/cannot-change-rvm-gemset-from-shell-script-via-rvm-gemset-use/6584010#6584010
 
-
+clear
 echo " "
-echo "enter project name or working title"
+echo -n "enter project name or working title: "
 read projectname
 mkdir ~/projects/$projectname
 echo "created directory ~/projects/$projectname"
@@ -20,8 +20,8 @@ touch readme.txt
 echo "$projectname by $USER" >> readme
 date >> readme
 echo "created readme.txt"
-mkdir copy conception design archive doc research temp
-echo "created sub directories: copy conception design  archive doc research temp"
+mkdir copy conception design archive doc rawmaterial research temp
+echo "created sub directories: copy conception design  archive doc research temp rawmaterial"
 echo " "
 rvm list
 echo "which ruby?"
